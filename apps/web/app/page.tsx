38e42380f9d9fe764300f08b1dbc79d760a3303b
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-07-11 19:35:48 -0400

    tw

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,22 @@
-import { Button } from '@showcase/ui/button';
+import Link from 'next/link';
+
+import { Button, buttonVariants } from '@showcase/ui/button';
 import { Header } from '@showcase/ui/header';
 
 export default function Page() {
   return (
     <>
-      <div>Web</div>
-      <Header text="Web" />
-      <Button>Button</Button>
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <Header text="Web" />
+        <Button>Button</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Link href="/about" className={buttonVariants({ variant: 'outline' })}>
+          About
+        </Link>
+        <Button variant="secondary" className="animate-in zoom-in duration-500">
+          Secondary
+        </Button>
+      </div>
     </>
   );
 }
