7c7d2b4f79a4cd1472b3c88840adfc3b31dc7c5e
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-07-12 13:46:17 -0400

    test

--- packages/ui/__tests__/button.test.tsx
+++ packages/ui/__tests__/button.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render } from '@testing-library/react';
 
-import Button from '../src/button';
+import { Button } from '../src/button';
 
 test('checks if button click updates the text', () => {
   const handleClick = jest.fn();
