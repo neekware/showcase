562ce12f08856e1029069d4d378e5572222ccc3d
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-07-14 19:57:18 -0400

    theme works

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,5 +3,5 @@ const tailwindPreset = require('tailwind-config/tailwind.config');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [tailwindPreset],
-  content: ['app/**/*.{ts,tsx}'],
+  content: ['../../packages/**/*.{tx,tsx}', '../**/*.{ts,tsx}'],
 };
