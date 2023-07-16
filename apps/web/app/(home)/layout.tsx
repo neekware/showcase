99b71e93dda918286f39ca9341256376fd8e3ff3
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-07-16 13:10:21 -0400

    Revert "css"
    This reverts commit 80ebf9286a5ef783ce91ea23bfbbf44caf04c453.

--- apps/web/app/(home)/layout.tsx
+++ apps/web/app/(home)/layout.tsx
@@ -13,7 +13,7 @@ interface HomeLayoutProps {
 export default async function HomeLayout({ children }: HomeLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="container z-40 dark:bg-transparent">
+      <header className="bg-background container z-40">
         <div className="flex h-20 items-center justify-between py-6">
           <LeftNav items={leftNavItems} siteConfig={siteConfig} />
           <nav>
@@ -29,7 +29,11 @@ export default async function HomeLayout({ children }: HomeLayoutProps) {
           </nav>
         </div>
       </header>
-      <main className="flex-1">{children}</main>
+      <main className="flex-1">
+        <div className="blue-gradient" role="presentation" />
+        <div className="purple-gradient" role="presentation" />
+        {children}
+      </main>
       <Footer siteConfig={siteConfig} />
     </div>
   );
