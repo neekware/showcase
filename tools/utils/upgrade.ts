c4db240e9bf90704f59f8e2cf603bb7523b8feed
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-09-24 19:29:38 -0400

    upgrades

--- tools/utils/upgrade.ts
+++ tools/utils/upgrade.ts
@@ -2,9 +2,17 @@ import { execute } from './util';
 
 // Define the packages you want to exclude
 const excludedPackages = ['jest-*', 'eslint-config-*', 'typescript'];
+console.log(`Running *****: ${excludedPackages}`);
 
-const excludeString = excludedPackages.map((pkg) => `!${pkg}`).join(',');
-const command = `yarn up '*'@latest,'${excludeString}'@latest`;
+async function main() {
+  const excludeString = excludedPackages.map((pkg) => `!${pkg}`).join(',');
+  const command = `yarn up '*'@latest,'${excludeString}'@latest`;
 
-console.log(`Running: ${command}`);
-execute(command);
+  // console.log(`Running *****: ${command}`);
+  // execute(command);
+}
+
+main().catch((err) => {
+  console.error(`Error upgrading`, err);
+  process.exit(111);
+});
