b86890022bb23a8f6dd08b6adf4835db8dd0881a
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-04-25 15:15:08 -0400

    transpile next libs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: [
-    "@repo/util",
-    "@repo/ui",
-    "@repo/logger",
-    "@repo/vendor-ui"
+    '@repo/util',
+    '@repo/ui',
+    '@repo/logger',
+    '@repo/vendor-ui',
   ],
 };
