18885535e89c775b20cab201431e8851a23f4e09
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-04 17:20:36 -0400

    theme

---
+++ apps/web/src/cfg/mobile.ts
@@ -0,0 +1,51 @@
+import { type MobileSettings } from '@repo/ui';
+
+export const mobileSettings: MobileSettings = {
+  navMain: [
+    {
+      title: 'Themes',
+      href: '/themes',
+    },
+    {
+      title: 'Examples',
+      href: '/examples',
+    },
+    {
+      title: 'GitHub',
+      href: 'https://github.com/neekware',
+      external: true,
+    },
+    {
+      title: 'X Twitter',
+      href: 'https://twitter.com/showcase_io',
+      external: true,
+    },
+  ],
+  navSidebar: [
+    {
+      title: 'Getting Started',
+      items: [
+        {
+          title: 'Introduction',
+          href: '/docs',
+          items: [],
+        },
+        {
+          title: 'About',
+          href: '/docs/about',
+          items: [],
+        },
+      ],
+    },
+    {
+      title: 'Dark Mode',
+      items: [
+        {
+          title: 'Next.js',
+          href: '/docs/dark-mode/next',
+          items: [],
+        },
+      ],
+    },
+  ],
+};
---
+++ apps/web/src/cfg/site.ts
@@ -0,0 +1,21 @@
+import { mdiGithub, mdiTwitter } from '@mdi/js';
+import { type SiteSettings } from '@repo/ui';
+
+export const siteSettings: SiteSettings = {
+  name: 'Showcase UI',
+  url: 'https://showcase.io',
+  ogImage: 'https://showcase.io/og.jpg',
+  description: 'Showcase your political self beautify on a unified portal.',
+  socials: [
+    {
+      title: 'X',
+      href: 'https://twitter.com/showcase_io',
+      icon: mdiTwitter,
+    },
+    {
+      title: 'GitHub',
+      href: 'https://github.com/neekware',
+      icon: mdiGithub,
+    },
+  ],
+};
