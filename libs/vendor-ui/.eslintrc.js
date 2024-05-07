56ae60e69af0406af1d0f0ac661e8bab31d439f1
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-07 11:45:25 -0400

    rename cfgs to config

--- libs/vendor-ui/.eslintrc.js
+++ libs/vendor-ui/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['@repo/eslint-cfg/react.js'],
+  extends: ['@repo/eslint-config/react.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
