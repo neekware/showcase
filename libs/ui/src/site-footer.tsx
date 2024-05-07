0a0c52c1307ced688c77ee851bc5199cc491c4a4
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-07 13:47:20 -0400

    fix footer

--- libs/ui/src/site-footer.tsx
+++ libs/ui/src/site-footer.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { Icon } from '@mdi/react';
 import { cn } from '@repo/util';
 import { buttonVariants } from '@repo/vendor-ui';
+import { NavSite } from './nav-site';
 import { type SiteSettings } from './site-types';
 
 interface MainHeaderProps {
@@ -9,40 +10,48 @@ interface MainHeaderProps {
   className?: string;
 }
 
-export function SiteFooter({ siteSettings, className }: MainHeaderProps) {
+const CURRENT_YEAR = new Date().getFullYear();
+
+export function SiteFooter({ className, siteSettings }: MainHeaderProps) {
   return (
-    <div className={cn('border-t py-6 md:px-8 md:py-0', className)}>
+    <div className={cn('border-t', className)}>
       <footer>
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-          <div className="text-muted-foreground text-center text-sm leading-loose md:text-left">
-            Built by Showcase UI, A Neekware Inc. Project.
-            {siteSettings.footerSocialLinks.map((sItem) => {
-              return (
-                <Link
-                  key={sItem.title}
-                  href={sItem.href}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="focus:outline-none"
-                >
-                  <div
-                    className={cn(
-                      buttonVariants({
-                        variant: 'link',
-                      }),
-                      'w-9 px-0'
-                    )}
+          <div className="text-muted-foreground container flex items-center justify-between text-center leading-loose">
+            <NavSite siteSettings={siteSettings} />
+            <div className="">
+              <div>© 2023-{CURRENT_YEAR} Neekware Inc.</div>
+              <div>Built by Showcase UI.</div>
+            </div>
+            <div className="">
+              {siteSettings.footerSocialLinks.map((sItem) => {
+                return (
+                  <Link
+                    key={sItem.title}
+                    href={sItem.href}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="focus:outline-none"
                   >
-                    {sItem.icon ? (
-                      <Icon path={sItem.icon} className="h-6 w-6" />
-                    ) : null}
-                    {sItem.label ? (
-                      <span className="sr-only">{sItem.label}</span>
-                    ) : null}
-                  </div>
-                </Link>
-              );
-            })}
+                    <div
+                      className={cn(
+                        buttonVariants({
+                          variant: 'link',
+                        }),
+                        'w-9 px-0'
+                      )}
+                    >
+                      {sItem.icon ? (
+                        <Icon path={sItem.icon} className="h-6 w-6" />
+                      ) : null}
+                      {sItem.label ? (
+                        <span className="sr-only">{sItem.label}</span>
+                      ) : null}
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
           </div>
         </div>
       </footer>
