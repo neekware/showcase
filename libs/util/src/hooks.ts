8f1797d9842235a9031e6f91b84360c10e75ca51
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-08 14:31:10 -0400

    states

--- libs/util/src/hooks.ts
+++ libs/util/src/hooks.ts
@@ -1,14 +1,20 @@
 import { useAtom } from 'jotai';
 import { type AppState, type ThemeType } from '@repo/dto';
-import { appStateAtom, DefaultStateSettings, themeAtom } from './state';
+import {
+  appStateAtom,
+  authAtom,
+  DefaultStateSettings,
+  profileAtom,
+  themeAtom,
+} from './state';
 
 export function useAppState() {
   const [state, setAppState] = useAtom(appStateAtom);
 
   const updateImmutable = (partialConfig: Partial<AppState>) => {
     setAppState({
-      ...state,
       ...DefaultStateSettings,
+      ...state,
       ...partialConfig,
     });
   };
@@ -27,4 +33,30 @@ export function useThemeState() {
   };
 
   return [theme, updateImmutable] as const;
+}
+
+export function useAuthState() {
+  const [auth, setAuthState] = useAtom(authAtom);
+
+  const updateImmutable = (partialConfig: Partial<ThemeType>) => {
+    setAuthState({
+      ...DefaultStateSettings.auth,
+      ...partialConfig,
+    });
+  };
+
+  return [auth, updateImmutable] as const;
+}
+
+export function useProfileState() {
+  const [profile, setAuthState] = useAtom(profileAtom);
+
+  const updateImmutable = (partialConfig: Partial<ThemeType>) => {
+    setAuthState({
+      ...DefaultStateSettings.profile,
+      ...partialConfig,
+    });
+  };
+
+  return [profile, updateImmutable] as const;
 }
