252b6dd89e11d172b6ce3d1e56b3574ebb63ecd9
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-11 15:41:43 -0400

    footer lineup

--- libs/ui/src/site-footer.tsx
+++ libs/ui/src/site-footer.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import Link from 'next/link';
-import { mdiMonitor, mdiWeatherNight, mdiWeatherSunny } from '@mdi/js';
 import { Icon } from '@mdi/react';
 import { type LinkItem, type SiteSettings, type Theme } from '@repo/dto';
-import { cn, useThemeState } from '@repo/util';
+import { cn } from '@repo/util';
 import { Button, buttonVariants } from '@repo/vendor-ui';
 import { NavSite } from './nav-site';
 import { ThemeSelector } from './theme-selector';
@@ -21,13 +20,6 @@ export function SiteFooter({
   themes,
   className,
 }: MainHeaderProps) {
-  const [mounted, setMounted] = React.useState(false);
-  const [theme, setThemeState] = useThemeState();
-
-  React.useEffect(() => {
-    setMounted(true);
-  }, []);
-
   return (
     <footer className={cn('border-t px-5 py-4', className)}>
       <nav
@@ -52,8 +44,8 @@ export function SiteFooter({
               <span className="h-2 w-2 rounded-full bg-green-500" />
               <small>All systems normal.</small>
             </Link>
-            {/* <!-- Social Section --> */}
-            <span className="mb-4 mt-auto flex">
+            {/* <!-- Social Section Desktop --> */}
+            <span className="mb-4 mt-auto hidden md:flex">
               <ul className="m-0 inline-flex list-none items-center p-0 pl-1">
                 {siteSettings.footer.social.map((sItem: LinkItem) => {
                   return (
@@ -95,7 +87,7 @@ export function SiteFooter({
           {/* <!-- Products Section --> */}
           <div className="flex flex-col">
             <h2 className="mb-2 text-base font-semibold">Product</h2>
-            <ul className="m-0 flex list-none space-y-1 p-0 pb-2 md:flex-col md:pb-0 xl:grid xl:grid-cols-2">
+            <ul className="md:border-non m-0 flex grid list-none space-y-1 border-t p-0 pb-2 pt-4 md:flex-col md:pb-0 xl:grid-cols-2">
               {siteSettings.footer.product.map((pItem: LinkItem) => {
                 return (
                   <li key={pItem.title}>
@@ -119,7 +111,7 @@ export function SiteFooter({
           {/* <!-- Resources Section --> */}
           <div className="flex flex-col">
             <h2 className="mb-2 text-base font-semibold">Resources</h2>
-            <ul className="m-0 flex list-none space-y-1 p-0 pb-2 md:flex-col md:pb-0 xl:grid xl:grid-cols-2">
+            <ul className="md:border-non m-0 grid list-none space-y-1 border-t p-0 pb-2 pt-4 md:flex-col md:pb-0 xl:grid-cols-2">
               {siteSettings.footer.resources.map((pItem: LinkItem) => {
                 return (
                   <li key={pItem.title}>
@@ -143,7 +135,7 @@ export function SiteFooter({
           {/* <!-- Company Section --> */}
           <div className="flex flex-col">
             <h2 className="mb-2 text-base font-semibold">Company</h2>
-            <ul className="m-0 flex list-none space-y-1 p-0 pb-2 md:flex-col md:pb-0 xl:grid xl:grid-cols-2">
+            <ul className="md:border-non m-0 grid list-none space-y-1 border-t p-0 pb-2 pt-4 md:flex-col md:pb-0 xl:grid-cols-2">
               {siteSettings.footer.company.map((pItem: LinkItem) => {
                 return (
                   <li key={pItem.title}>
@@ -167,72 +159,55 @@ export function SiteFooter({
           {/* <!-- Company Section --> */}
           <div className="flex flex-col">
             <h2 className="mb-2 text-base font-semibold">Preferences</h2>
-            <div className="m-0 flex list-none space-y-1 p-0 pb-2 md:flex-col md:pb-0 xl:grid xl:grid-cols-3">
-              <div className="grid grid-cols-3 gap-2">
-                {mounted ? (
-                  <>
-                    <Button
-                      onClick={() => {
-                        setThemeState({ mode: 'light' });
-                      }}
-                      variant="ghost"
-                      className={cn(
-                        'flex size-9 items-center justify-center rounded-full',
-                        theme.mode === 'light'
-                          ? 'text-foreground/80 border'
-                          : 'text-foreground/60'
-                      )}
-                    >
-                      <div className="text-primary">
-                        <Icon path={mdiWeatherSunny} size={1} />
-                      </div>
-                      <span className="sr-only">Light theme</span>
-                    </Button>
-
-                    <Button
-                      onClick={() => {
-                        setThemeState({ mode: 'system' });
-                      }}
-                      variant="ghost"
-                      className={cn(
-                        'flex size-9 items-center justify-center rounded-full',
-                        theme.mode === 'system'
-                          ? 'text-foreground/80 border'
-                          : 'text-foreground/60'
-                      )}
-                    >
-                      <div className="text-primary">
-                        <Icon path={mdiMonitor} size={1} />
-                      </div>
-                      <span className="sr-only">System theme</span>
-                    </Button>
-
-                    <Button
-                      onClick={() => {
-                        setThemeState({ mode: 'dark' });
-                      }}
-                      variant="ghost"
-                      className={cn(
-                        'flex size-9 items-center justify-center rounded-full',
-                        theme.mode === 'dark'
-                          ? 'text-foreground/80 border'
-                          : 'text-foreground/60'
-                      )}
-                    >
-                      <div className="text-primary">
-                        <Icon path={mdiWeatherNight} size={1} />
-                      </div>
-                      <span className="sr-only">Dark theme</span>
-                    </Button>
-                    <ThemeSelector
-                      themes={themes}
-                      className="flex size-8 items-center justify-center rounded-full"
-                    />
-                  </>
-                ) : null}
-              </div>
-            </div>
+            <ul className="md:border-non m-0 grid list-none space-y-1 border-t p-0 pb-2 pt-4 md:flex-col md:pb-0 xl:grid-cols-2">
+              <li key="theme" className="flex items-center">
+                <ThemeSelector
+                  themes={themes}
+                  className="text-foreground/60 hover:text-foreground -ml-2 size-8 rounded-full transition-colors focus:outline-none"
+                />
+                <div className="capitalize">Themes</div>
+              </li>
+            </ul>
           </div>
+
+          {/* <!-- Social Section On Mobile --> */}
+          <span className="mb-4 mt-auto flex md:hidden">
+            <ul className="m-0 ml-[-6] inline-flex list-none items-center p-0">
+              {siteSettings.footer.social.map((sItem: LinkItem) => {
+                return (
+                  <li key={sItem.title} className="m-0 rounded-full p-0 pr-2">
+                    <Button
+                      variant="ghost"
+                      className="flex size-8 items-center justify-center rounded-full"
+                    >
+                      <Link
+                        href={sItem.href}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="focus:outline-none"
+                      >
+                        <div
+                          className={cn(
+                            buttonVariants({
+                              variant: 'link',
+                            }),
+                            'w-9 px-0'
+                          )}
+                        >
+                          {sItem.icon ? (
+                            <Icon path={sItem.icon} className="h-6 w-6" />
+                          ) : null}
+                          {sItem.label ? (
+                            <span className="sr-only">{sItem.label}</span>
+                          ) : null}
+                        </div>
+                      </Link>
+                    </Button>
+                  </li>
+                );
+              })}
+            </ul>
+          </span>
         </div>
       </nav>
     </footer>
