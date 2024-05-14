b90cc675d84af8ef939218d03ab574f58c441a29
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-14 17:16:23 -0400

    comments

--- libs/util/src/hooks.ts
+++ libs/util/src/hooks.ts
@@ -2,9 +2,19 @@ import { useAtom } from 'jotai';
 import { type AppState, type ThemeState } from '@repo/dto';
 import { appStateAtom, authAtom, DefaultStateSettings, profileAtom, themeAtom } from './state';
 
+/**
+ * Hook to access and update the application state
+ *
+ * @returns An array containing the current state and an update function
+ */
 export function useAppState() {
   const [state, setAppState] = useAtom(appStateAtom);
 
+  /**
+   * Function to update the application state immutably
+   *
+   * @param partialConfig - Partial configuration to update the state
+   */
   const updateImmutable = (partialConfig: Partial<AppState>) => {
     setAppState({
       ...DefaultStateSettings,
@@ -16,9 +26,19 @@ export function useAppState() {
   return [state, updateImmutable] as const;
 }
 
+/**
+ * Hook to access and update the theme state
+ *
+ * @returns An array containing the current theme and an update function
+ */
 export function useThemeState() {
   const [theme, setThemeState] = useAtom(themeAtom);
 
+  /**
+   * Function to update the theme state immutably
+   *
+   * @param partialConfig - Partial configuration to update the theme
+   */
   const updateImmutable = (partialConfig: Partial<ThemeState>) => {
     setThemeState({
       ...DefaultStateSettings.theme,
@@ -29,9 +49,19 @@ export function useThemeState() {
   return [theme, updateImmutable] as const;
 }
 
+/**
+ * Hook to access and update the authentication state
+ *
+ * @returns An array containing the current auth state and an update function
+ */
 export function useAuthState() {
   const [auth, setAuthState] = useAtom(authAtom);
 
+  /**
+   * Function to update the auth state immutably
+   *
+   * @param partialConfig - Partial configuration to update the auth state
+   */
   const updateImmutable = (partialConfig: Partial<ThemeState>) => {
     setAuthState({
       ...DefaultStateSettings.auth,
@@ -42,9 +72,19 @@ export function useAuthState() {
   return [auth, updateImmutable] as const;
 }
 
+/**
+ * Hook to access and update the profile state
+ *
+ * @returns An array containing the current profile and an update function
+ */
 export function useProfileState() {
   const [profile, setAuthState] = useAtom(profileAtom);
 
+  /**
+   * Function to update the profile state immutably
+   *
+   * @param partialConfig - Partial configuration to update the profile
+   */
   const updateImmutable = (partialConfig: Partial<ThemeState>) => {
     setAuthState({
       ...DefaultStateSettings.profile,
