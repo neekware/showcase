ddc9a3721ea97a66e5351fe38658b02d31aeb484
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-15 22:36:37 -0400

    init

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   transpilePackages: ['@repo/util', '@repo/ui', '@repo/logger', '@repo/vendor-ui'],
-};
+});
