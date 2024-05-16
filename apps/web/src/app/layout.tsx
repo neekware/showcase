2de1d0e835809b895ca1f63c82ab76f15bf59c78
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-15 23:05:50 -0400

    pwa works

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -11,6 +11,17 @@ export const generateMetadata = () => metaSettings;
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en" dir="ltr">
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="icon" type="image/ico" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover"
+        />
+        {/* <meta name="theme-color" content="#18181b" media="(prefers-color-scheme: dark)" />
+        <meta name="theme-color" content="#f4f4f5" /> */}
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={fontRoboto.className}>
         <ThemeProvider
           attribute="class"
