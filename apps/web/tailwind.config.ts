bf4eb867b1b6c290a3d2290a71e3003ffbefd787
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-23 16:15:54 -0400

    rename tailwind

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -5,9 +5,9 @@ const config: Config = {
   ...baseConfig,
   content: [
     './src/app/**/*.{ts,tsx,html}',
-    '../../libs/util/src/**/*.{ts,tsx,html}',
-    '../../libs/vendor-ui/src/**/*.{ts,tsx,html}',
-    '../../libs/ui/src/**/*.{ts,tsx,html}',
+    '../../libs/nx-util/src/**/*.{ts,tsx,html}',
+    '../../libs/nx-ui-vendor/src/**/*.{ts,tsx,html}',
+    '../../libs/nx-ui/src/**/*.{ts,tsx,html}',
   ],
 };
 export default config;
