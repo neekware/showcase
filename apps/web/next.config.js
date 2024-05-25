09f87cb37cb08617dd69026c9c5b42fd94e36656
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-25 12:25:27 -0400

    concurrency, transpile off

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,14 +2,14 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: [
-    '@repo/nx-env',
-    '@repo/ag-db',
-    '@repo/ag-dto',
-    '@repo/ag-logger',
-    '@repo/ag-store',
-    '@repo/nx-auth',
-    '@repo/nx-ui',
-    '@repo/nx-ui-vendor',
-    '@repo/nx-util',
+    // '@repo/nx-env',
+    // '@repo/ag-db',
+    // '@repo/ag-dto',
+    // '@repo/ag-logger',
+    // '@repo/ag-store',
+    // '@repo/nx-auth',
+    // '@repo/nx-ui',
+    // '@repo/nx-ui-vendor',
+    // '@repo/nx-util',
   ],
 };
