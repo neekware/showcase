a957d956f5b502dce3c6776e5c8ae7d91565f2c6
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-05-26 00:13:36 -0400

    build list

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,15 +2,16 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: [
+    '@repo/ag-dto',
     '@repo/ag-env',
     '@repo/ag-db',
-    '@repo/ag-dto',
-    '@repo/ag-user',
     '@repo/ag-logger',
+    '@repo/ag-user',
     '@repo/ag-store',
     '@repo/nx-auth',
+    '@repo/nx-util',
+    '@repo/nx-ui-form',
     '@repo/nx-ui',
     '@repo/nx-ui-vendor',
-    '@repo/nx-util',
   ],
 };
