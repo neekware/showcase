615c64c051429bb78acdaa72ee368afc58a8f6c0
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2024-06-06 21:29:04 -0400

    version during build

--- apps/web/src/app/dash/page.tsx
+++ apps/web/src/app/dash/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { log } from '@repo/ag-logger';
 import { Button } from '@repo/nx-ui-vendor';
+import buildInfo from '../../cfg/config.json';
 
 export const metadata = {
   title: 'Dash | Showcase',
@@ -10,16 +11,31 @@ export default function Dash(): JSX.Element {
   log('Hey! This is the Dash page.');
 
   return (
-    <div className="flex min-h-screen flex-col items-center space-y-2 p-2">
-      <div className="mx-auto flex max-w-5xl gap-2 text-2xl">
-        <div>Dashboard</div>
-        <Link href="/auth/login">
-          <Button variant="default">Login</Button>
-        </Link>
-        <Link href="/products">
-          <Button variant="danger">Products</Button>
-        </Link>
-      </div>
+    <div className="flex flex-col items-center space-y-2 p-2">
+      <section className="py-12 md:py-24 lg:py-32">
+        <div className="container px-4 md:px-6">
+          <div className="mx-auto max-w-4xl space-y-6 text-left">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+              Dashboard - Showcase UI
+            </h1>
+            <p className="text-gray-500 md:text-xl/relaxed dark:text-gray-400">
+              We are a leading provider of innovative solutions for political individuals of all
+              levels. Explore our products and services to see how we can help your campaign thrive.
+            </p>
+
+            <div>{JSON.stringify(buildInfo, null, 2)}</div>
+
+            <div className="flex gap-2">
+              <Link href="/auth/login">
+                <Button variant="default">Login</Button>
+              </Link>
+              <Link href="/products">
+                <Button variant="danger">Products</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
