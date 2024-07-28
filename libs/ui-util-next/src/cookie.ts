ead47e9e25ec5f8526d6f444e9d3e825622ce9c1
Val (AvidCoder) <un33kvu@gmail.com>
2024-07-28 10:32:16 -0400

    doc

--- libs/ui-util-next/src/cookie.ts
+++ libs/ui-util-next/src/cookie.ts
@@ -1,6 +1,11 @@
 import { type JWTPayload, JWTService } from '@lib/data-net-shared';
 
-export const getCookie = (name: string): string | null => {
+/**
+ * Get the value of a cookie
+ * @param name name of the cookie
+ * @returns value of the cookie
+ */
+export const getCookie = (name: string): string | undefined => {
   const nameEQ = `${name}=`;
   const ca = document.cookie.split(';');
   for (let i = 0; i < ca.length; i++) {
@@ -8,13 +13,18 @@ export const getCookie = (name: string): string | null => {
     while (c?.charAt(0) === ' ') c = c.substring(1, c.length);
     if (c?.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
   }
-  return null;
+  return undefined;
 };
 
+/**
+ * Check if a cookie exists and is valid
+ * @param name name of the cookie
+ * @param value value of the cookie
+ * @param days days to expire
+ */
 export const isSessionValid = async (cookieName: string): Promise<JWTPayload | undefined> => {
   const cookie = getCookie(cookieName);
   if (cookie) {
-    console.log('cookie', cookie);
     const jwtPrev = await JWTService.decrypt(cookie);
     if (jwtPrev.success && jwtPrev.data) {
       return jwtPrev.data;
